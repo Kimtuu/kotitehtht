0f4c7788563e74e00e3966235ac82d3b9283a7d0
kimtuu <lofgrenkim@outlook.com>
2022-09-01 13:15:16 +0300

    tuuma->cm

--- Task_4/Task_4_2.py
+++ Task_4/Task_4_2.py
@@ -1,14 +1,16 @@
-
 ##Kirjoita ohjelma, joka muuntaa tuumia senttimetreiksi niin kauan kunnes käyttäjä antaa negatiivisen tuumamäärän.
 # Sen jälkeen ohjelma lopettaa toimintansa.
 #1tuuma=2.54
 #kysytään käyttäjältä tuumat
-inches = float (input("anna tuuma(t):"))
 
+inches = float (input("anna tuuma(t):"))
+#rakennnetaan while loopin sisään säännöt
 while inches >=0 :
     cm = inches * 2.54
     print(inches, "inches=", cm, "cm")
+    print("voit keskeyttää muuntamisen syöttämällä 0 pienemmän luvun")
     inches = float (input("anna tuuma(t):"))
+#while loopin ulkopuolelle mennään kun inches <= 0
 print("muuntaminen keskeytetty")
 
 
