5251ff99e8e2f4f7b25b943c98a6243c4574711b
kimtuu <lofgrenkim@outlook.com>
2022-09-01 15:40:34 +0300

    salasana

---
+++ Task_4/Task_4_5.py
@@ -0,0 +1,29 @@
+#Kirjoita ohjelma, joka kysyy käyttäjältä käyttäjätunnuksen ja
+#salasanan.
+#Jos jompikumpi tai molemmat ovat väärin, tunnus ja salasana kysytään uudelleen.
+#Tätä jatketaan kunnes kirjautumistiedot ovat oikein tai väärät tiedot on syötetty viisi kertaa.
+#Edellisessä tapauksessa tulostetaan Tervetuloa ja jälkimmäisessä Pääsy evätty.
+#(Oikea käyttäjätunnus on python ja salasana rules).
+
+username_input = str
+password_input = str
+
+#while
+tries = 5
+
+#username_input = str(input("username:"))
+#password_input = str(input("Password"))
+
+while tries > 0 :
+    username_input = str(input("username:"))
+    password_input = str(input("Password"))
+    if username_input == "python" and password_input == "rules":
+        print("Tervetuloa")
+        break
+
+     #username_input != username or password_input != password:
+        #print("pääsy evätty")
+    tries-=1
+if tries == 0 and username_input != "python" or password_input != "rules":
+    print("pääsy evätty")
+
