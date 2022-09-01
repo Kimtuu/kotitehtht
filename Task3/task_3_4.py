c680749ba0d8e13512ae89c81661b312cd041c6a
kimtuu <lofgrenkim@outlook.com>
2022-09-01 12:29:16 +0300

    karkausvuosi

--- Task3/task_3_4.py
+++ Task3/task_3_4.py
@@ -1,7 +1,7 @@
 #Kirjoita ohjelma, joka kysyy vuosiluvun ja ilmoittaa, onko annettu vuosi karkausvuosi.
 #Vuosi on karkausvuosi, jos se on jaollinen neljällä.
 #Sadalla jaolliset vuodet ovat karkausvuosia vain jos ne ovat jaollisia myös neljälläsadalla.
-
+##
 year = int (input("anna vuosiluku:"))
 if (year % 400 == 0) and (year % 100 == 0) :
     print("kyseessä on karkausvuosi")
