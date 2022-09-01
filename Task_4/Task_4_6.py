8a30fefcdf74d05028492ba357452043c31a6e4a
kim <lofgrenkim@outlook.com>
2022-09-01 20:35:26 +0300

    pii likiarvo D:

---
+++ Task_4/Task_4_6.py
@@ -0,0 +1,21 @@
+import random
+import math
+
+#kysytään nuolia, jotka osuu sisälle
+nuolet_sisällä = int(input("Anna ympyrän sisälle osuneet nuolet:"))
+
+#kysytään kaikkien heitettyjen nuolien arvoja
+nuolet_heitetty = int(input("Anna heitettyjen nuolien kokonaismäärä:"))
+
+#iteroidaan nuolien määrälle
+for i in range(0,nuolet_heitetty):
+    x2 = random.uniform(-1.0,1.0)**2
+    y2 = random.uniform(-1.0,1.0)**2
+if x2+y2 < 1.0:
+
+    nuolet_sisällä += 1
+
+pii = (float (nuolet_sisällä)/(nuolet_heitetty)) * 4
+
+print("piin liki arvo on", pii)
+
