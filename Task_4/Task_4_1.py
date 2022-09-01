694eebce7b0515791da92089e7e37326ecc11a39
kimtuu <lofgrenkim@outlook.com>
2022-09-01 11:00:43 +0300

    Karkausvuosi

---
+++ Task_4/Task_4_1.py
@@ -0,0 +1,8 @@
+#Kirjoita while-toistorakennetta käyttävä ohjelma,
+#joka tulostaa kolmella jaolliset luvut väliltä 1..1000.
+
+x= int(1)
+while x< 1000 :
+    x+=1
+    if x%3==0:
+        print(x)
