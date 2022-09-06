276ccbbfc050e5539c2909f69386872bd835e1af
kim <lofgrenkim@outlook.com>
2022-09-06 10:52:03 +0300

    polttoaine muunnos

---
+++ Task_6/Task_6.3.py
@@ -0,0 +1,11 @@
+
+def gallona_litraksi(bensa):
+    litra = bensa / 0.264172052
+    return litra
+
+gallona = int (input("anna gallonat:"))
+
+while gallona > 0:
+    muunnos = gallona_litraksi(gallona)
+    print(f"US gal, {muunnos} ")
+    gallona = int(input("lopeta syöttämällä -1 tai anna gallonat:"))
