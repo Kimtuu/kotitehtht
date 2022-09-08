bf6fb4f126006e2d2f5ea690b1ee4ef49cff5eb4
kimtuu <lofgrenkim@outlook.com>
2022-09-08 13:43:48 +0300

    alkuluku

--- Task_5/Task_5.3.py
+++ Task_5/Task_5.3.py
@@ -5,9 +5,9 @@ numero = int (input("Anna tarkastettava numero:"))
 if numero > 1:
     for i in range(2,int(numero/2+1)):
         if (numero % i) == 0:
-            print(numero, "ei ole alkunumero")
+            print(numero, "ei ole alkuluku")
             break
     else:
-        print(numero, "on alkunumero")
+        print(numero, "on alkuluku")
 else:
-    print(numero, "ei ole alkunumero")
+    print(numero, "ei ole alkuluku")
