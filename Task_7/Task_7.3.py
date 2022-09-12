c7879b9a5a14a140af3b57d35ef39fa4d5976b82
kimtuu <lofgrenkim@outlook.com>
2022-09-12 10:50:58 +0300

    lentokenttä haku

--- Task_7/Task_7.3.py
+++ Task_7/Task_7.3.py
@@ -1,8 +1,19 @@
-lentokentät = {"EFHK":"Helsinki-Vantaa",
-}
+lentokentät = {"EFHK" : "Helsinki-Vantaa"}
+user = int(input("1 =hae lentokenttää  2=lisää lentokenttä   3=lopeta  \nvalitse: "))
+while user != 3:
+    if user == 1:
+        icao_search = str(input("Hae lentokenttää ICAO-koodilla: ")).upper()
+        if icao_search in lentokentät:
+                print(f"Lentokenttä {icao_search} on {lentokentät[icao_search]}")
+        elif icao_search not in lentokentät:
+            print("Lento kenttää ei ole tietokannassa")
+    if user == 2:
+        ICAO = str(input("Anna ICAO: ")).upper()
+        airport_name = str(input("Lentokentän nimi: "))
+        lentokentät[ICAO] = airport_name
+    user = int(input("1 =hae lentokenttää  2=lisää lentokenttä   3=lopeta  \nvalitse: "))
 
-user = int(input("1 = hae lentokenttää\n2 = lisää lentokenttä\n3 = lopeta\nvalitse: "))
-if user == 1:
-    icao_search = str(input("Hae lentokenttää ICAO-koodilla:"))
-    if icao_search in lentokentät:
-        print(f"")
+
+
+print("ohjelma lopetettu")
+print(lentokentät)
