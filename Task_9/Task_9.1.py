dc4b7226d90aec16b1108cab4d3129dec02b5177
kimtuu <lofgrenkim@outlook.com>
2022-10-25 18:04:26 +0300

    9.1 autospeksi printtaus, hiottu pois PEP8 huomautuksia

--- Task_9/Task_9.1.py
+++ Task_9/Task_9.1.py
@@ -1,23 +1,22 @@
-#Autoluokka, jossa ominaisuudet; rekkari, huippunopeus, tämänhetkinen nopeus,
-#ja kuljettu matka
+# Autoluokka, jossa ominaisuudet; rekkari, huippunopeus, tämänhetkinen nopeus,
+# ja kuljettu matka
 
 class Car:
-    def __init__ (self, reg, top_speed):
-        self.reg = reg
+    def __init__(self, registerplate, top_speed):
+        self.reg = registerplate
         self.top_speed = top_speed
-        self.current_speed = 0
-        self.d_travelled = 0
+        self.speed = 0
+        self.Tripmeter = 0
 
     def print_info(self):
         print(f"Auton {self.reg}, "
               f"huippunopeus on:{self.top_speed}, "
-              f"matkamittari: {self.d_travelled}, "
-              f"Tämän hetkinen nopeus: {self.current_speed}.")
+              f"matkamittari: {self.Tripmeter}, "
+              f"Tämän hetkinen nopeus: {self.speed}.")
+
 
 car1 = Car("ABC-123", 142)
 car2 = Car("XYZ-987", 150)
 
 car1.print_info()
 car2.print_info()
-
-
