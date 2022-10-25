b6f2897caca38510ecf1ee6cb139714d6f7037e8
kimtuu <lofgrenkim@outlook.com>
2022-10-25 18:53:42 +0300

    9.2 Kiihdyttely ja jarruttelu ominaisuus + tämän hetkinen nopeus

--- Task_9/Task_9.2.py
+++ Task_9/Task_9.2.py
@@ -1,32 +1,33 @@
 class Car:
-    def __init__ (self, reg, top_speed):
+
+    def __init__(self, reg, top_speed):
         self.reg = reg
         self.top_speed = top_speed
-        self.current_speed = 0
-        self.d_travelled = 0
+        self.speed = 0
+        self.tripmeter = 0
 
     def print_info(self):
         print(f"Auton {self.reg}, "
               f"huippunopeus on:{self.top_speed}Km/h, "
-              f"matkamittari: {self.d_travelled}Km, "
-              f"Tämän hetkinen nopeus: {self.current_speed}Km/h")
+              f"matkamittari: {self.tripmeter}Km, "
+              f"Tämän hetkinen nopeus: {self.speed}Km/h")
 
     def accelerate(self, speed_change):
-        if 0 < self.current_speed + speed_change <= self.top_speed:
-            self.current_speed = self.current_speed + speed_change
-        elif: self.current_speed + speed_change <= 0
-            self.current_speed = 0
-        #TODO: jos uusinopeus > maksimi: nopeus = maksimi
+        if 0 < self.speed + speed_change <= self.top_speed:
+            self.speed = self.speed + speed_change
+        elif self.speed + speed_change <= 0:
+            self.speed = 0
+        elif self.speed + speed_change > self.top_speed:
+            self.speed = self.top_speed
+
 
 car1 = Car("ABC-123", 142)
-car2 = Car("XYZ-987", 150)
-car1.accelerate(2330)
+
+car1.accelerate(30)
 car1.print_info()
-car1.accelerate(15)
+car1.accelerate(70)
+car1.print_info()
+car1.accelerate(50)
 car1.print_info()
 car1.accelerate(-200)
 car1.print_info()
-
-#car2.print_info()
-
-
