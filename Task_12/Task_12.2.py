071473d8eb2ffc14fa7047e05c219b8f758d8ca6
kimtuu <lofgrenkim@outlook.com>
2022-11-03 19:16:01 +0200

    12.1 OpenWeater API

---
+++ Task_12/Task_12.2.py
@@ -0,0 +1,20 @@
+import requests
+# import json
+
+base_url = "https://api.openweathermap.org/data/2.5/weather?"
+api_key = "e76965d853e439fea2c006d1e2545908"
+city = input("Syötä kaupunki: ")
+
+url = base_url + "appid=" + api_key + "&q=" + city + "&units=metric&lang=fi"
+
+try:
+    response = requests.get(url)
+    if response.status_code == 200:
+        json_response = response.json()
+        temp = json_response['main']['temp']
+        description = json_response['weather'][0]['description']
+        print(f"{city}\n"
+              f"Lämpötila: {temp}°C\n"
+              f"Sää: {description}")
+except requests.exceptions.RequestException as e:
+    print("hakua ei voitu suorittaa")
